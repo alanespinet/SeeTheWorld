76d863eb64dcde0768a12f99cb344e3b250b8b12
alanespinet <aelluvetaa@gmail.com>
2017-01-02 23:26:12 -0500

    FrontComplete

--- js/locationsHandler.js
+++ js/locationsHandler.js
@@ -1,11 +1,11 @@
-$('#locations').on('click', '.card', function(){
+$('#locations').on('click', '.card', function(e){
   var w = $(window).innerWidth();
   $('#locations').css('left', -w + 'px');
   var rindex = $(this).children('location-dir').attr('rindex');
 
   loadLocationInfo(rindex);
-  return false;
 });
+
 
 function loadLocationInfo(pLocationIndex){
   $.ajax('pseudoData/locations.json', {
@@ -16,6 +16,27 @@ function loadLocationInfo(pLocationIndex){
       $('#desc').text(locationToShow.description);
       $('#imgCar1').attr('src', `img/locations/${locationToShow.images[0]}`);
       $('#imgCar2').attr('src', `img/locations/${locationToShow.images[1]}`);
+
+      $('.location-image').off('click');
+      $('.location-image').on('click', function(){
+        var $divOver = $('<div class="overlay container"></div>');
+        var src = $(this).attr('src');
+
+        $divOver.css('background-color', 'rgba(50, 50, 50, 0.7)');
+        $divOver.width($(window).innerWidth());
+        $divOver.height($(window).innerHeight());
+        $divOver.css('position', 'fixed');
+        $divOver.css('left', '0');
+        $divOver.css('top', '0');
+
+        var $image = $('<img class="img-restricted" />');
+        $image.attr('src', src);
+        $divOver.append($image);
+
+        $('body').append($divOver);
+        return false;
+      });
+
       $('#desc1').text(locationToShow.description1);
       $('#price').html('<i class="pe-7s-credit pe-2x pe-va"></i>Prize: ' + locationToShow.price);
       $('#duration').html('<i class="pe-7s-clock pe-2x pe-va"></i>Duration: ' + locationToShow.duration);
@@ -25,8 +46,6 @@ function loadLocationInfo(pLocationIndex){
       if(locationToShow.discounts == "No"){ $('.locs .pe-7s-like2').css('color', 'tomato'); }
       else { $('.locs .pe-7s-like2').css('color', 'forestgreen'); }
 
-      // $('#imgCar3').attr('src', `img/locations/${locationToShow.images[2]}`);
-
       $('#locationName').text(locationToShow.name);
     },
     error: function(error){
@@ -34,4 +53,19 @@ function loadLocationInfo(pLocationIndex){
     }
   });
 
+
+  $('#locations-button').off('click')
+  .on('click', function(){
+    location.href='#register';
+  });
+
+
+  $('body').on('click', '.overlay', function(){
+    $(this).remove();
+  });
+
+
+  $('#back-arrow').on('click', function(){
+    $('#locations').css('left', '0px');
+  });
 }
